c401480a00df594ca8bc6ef2ffe66f074fd38e13
Mario Kaufmann <mario.kaufmann@visana.ch>
2020-10-07 11:20:06 +0200

    Add chapter 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Reveal from 'reveal.js';
+import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm';
+import RevealNotes from 'reveal.js/plugin/notes/notes.esm';
 
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/plugin/highlight/monokai.css';
@@ -10,11 +13,13 @@ import './zuehlke-toc.css';
 
 import toc from './pages/toc.html';
 import chapter1 from './pages/01.html';
+import chapter2 from './pages/02.html';
 
 document.querySelector('.toc').innerHTML = `<div>${toc}</div>`;
 
 const chapters = [
     chapter1,
+    chapter2,
 ];
 
 function loadChapter(index) {
@@ -23,9 +28,13 @@ function loadChapter(index) {
     document.querySelector('.toc').innerHTML = '';
     document.querySelector('.slides-container').innerHTML = `<div class="reveal"><div class="slides">${chapter}</div></div>`;
 
-    const deck = new Reveal(document.querySelector('.reveal'));
-    deck.initialize();
+    const deck = new Reveal(document.querySelector('.reveal'), {
+        history: true,
+        plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+    });
+    deck.initialize().then(() => deck.getPlugin( 'highlight' ).hljs.initHighlightingOnLoad());
+    // hljs.initHighlightingOnLoad();
 }
 
-loadChapter(1);
+loadChapter(2);
 
